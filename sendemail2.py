f239231760dbce1135b70d9dd9215354761254fa
Mayank Pratap Singh <MayankPratap@users.noreply.github.com>
2016-03-18 13:13:10 +0530

    Added script to send email with subject

---
+++ sendemail2.py
@@ -0,0 +1,30 @@
+#!python3
+
+# This script sends email to a person in a clean way 
+
+# You have to allow "less secure apps" on your gmail account before running this script.
+
+import smtplib
+
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+fromaddr = " XYZ@gmail.com "
+toaddr = " ZYX@gmail.com "
+
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = " Sending mail using terminal, just for fun "
+
+body = " Hey ZYX , whats going on ?? I was learning how to send mail from terminal using Python , So I just mailed you ...  Sorry for disturbing :( "
+
+msg.attach(MIMEText(body,'plain'))
+
+server=smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(fromaddr,"Your Password")
+text=msg.as_string()
+server.sendmail(fromaddr,toaddr,text)
+server.quit()
+
