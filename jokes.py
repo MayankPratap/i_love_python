a9483298d2476ae3c150a61c8485335474104f51
Mayank Pratap Singh <MayankPratap@users.noreply.github.com>
2016-03-19 23:00:43 +0530

    Create jokes.py

---
+++ jokes.py
@@ -0,0 +1,18 @@
+# A very simple game in python 
+
+print('What do you get when you cross a snowman with a vampire?')
+input()
+print('frostbite!')
+print()
+print('What dou dentists call a astronaut\'s cavity?')
+input()
+print('A black hole!')
+print()
+print('Knock Knock')
+input()
+print("Who's there?")
+input()
+print('Interrupting cow.')
+input()
+print('Interrupting cow wh',end='')
+print('-MOO!')
